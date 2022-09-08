ce2c584dba605d49977d227ffac216e5b3b3707a
hafidz <hafidz.bdt@gmail.com>
2022-09-08 15:24:54 +0700

    8/9/2022

--- resources/views/home/auth/register.blade.php
+++ resources/views/home/auth/register.blade.php
@@ -20,67 +20,87 @@
 @extends('template.app')
 
 @section('template')
-    <div class="container-fluid register">
-        <div class="row align-items-center">
-            <div class="col-md-6 p-0 mb-md-0 mb-3">
-                <img src="{{ asset('img/bg-login.png') }}" alt="Login" class="register-bg w-100">
-            </div>
-            <div class="col-md-6">
-                <h3 class="text-center">Sign up as Shelter</h3>
-                <form action="" method="post">
-                    @csrf
-                    <div class="row justify-content-center mt-4">
-                        <div class="col-md-6">
-                            <div class="mb-2">
-                                <label for="name">Full Name</label>
-                                <input type="text" name="" id="name" class="form-control"
-                                    autocomplete="false" readonly onfocus="this.removeAttribute('readonly');">
-                            </div>
-                            <div class="mb-2">
-                                <label for="user">Email</label>
-                                <input type="email" name="" id="user" class="form-control"
-                                    autocomplete="false" readonly onfocus="this.removeAttribute('readonly');">
-                            </div>
-                            <div class="mb-2">
-                                <label for="user">Phone Number</label>
-                                <input type="tel" name="" id="phone" class="form-control"
-                                    placeholder="+6281xxxxxx" onfocus="phoneNumber()">
-                            </div>
-                            <div class="mb-2">
-                                <label for="pass">Password</label>
-                                <input type="password" name="" id="pass" class="form-control"
-                                    autocomplete="false">
-                            </div>
-                            <div class="mb-2">
-                                <label for="pass">Password Confirmation</label>
-                                <input type="password" name="" id="pass" class="form-control"
-                                    autocomplete="false">
-                            </div>
-                            <button class="btn btn-secondary mt-3 w-100" type="submit"><i
-                                    class="bi bi-box-arrow-in-right me-1"></i>
-                                Sign Up Now</button>
-                            <div class="mt-5 text-center text-muted">
-                                Already have an account?
-                                <a href="/login" class="text-decoration-none fw-bold text-muted">
-                                    Login here
-                                </a>
+    <div id="app">
+        <div class="container-fluid register">
+            <div class="row align-items-center">
+                <div class="col-md-6 p-0 mb-md-0 mb-3">
+                    <img src="{{ asset('img/bg-login.png') }}" alt="Login" class="register-bg w-100">
+                </div>
+                <div class="col-md-6">
+                    <h3 class="text-center">Sign up as Shelter</h3>
+                    <form action="" method="post">
+                        @csrf
+                        <div class="row justify-content-center mt-4">
+                            <div class="col-md-6">
+                                <div class="mb-2">
+                                    <label for="name">Full Name</label>
+                                    <input type="text" name="" id="name" class="form-control"
+                                        autocomplete="false" readonly onfocus="this.removeAttribute('readonly');">
+                                </div>
+                                <div class="mb-2">
+                                    <label for="user">Email</label>
+                                    <input type="email" name="" id="user" class="form-control"
+                                        autocomplete="false" readonly onfocus="this.removeAttribute('readonly');">
+                                </div>
+                                <div class="mb-2">
+                                    <label for="user">Phone Number</label>
+                                    <input type="tel" name="" id="phone" class="form-control"
+                                        placeholder="+6281xxxxxx" onfocus="phoneNumber()">
+                                </div>
+                                <div class="mb-2">
+                                    <label for="pass">Password</label>
+                                    <div class="position-relative overflow-hidden">
+                                        <div class="position-absolute pointer" style="right:10px; top:10px; z-index:999"
+                                            onclick="vPassword('pass')">
+                                            <i class="bi bi-eye-slash" id="passIcon"></i>
+                                        </div>
+                                        <input type="password" name="" id="pass" class="form-control"
+                                            autocomplete="false">
+                                    </div>
+                                </div>
+                                <div class="mb-2">
+                                    <label for="pass">Password Confirmation</label>
+                                    <div class="position-relative overflow-hidden">
+                                        <div class="position-absolute pointer" style="right:10px; top:10px; z-index:999"
+                                            onclick="vPassword('pass_confirm')">
+                                            <i class="bi bi-eye-slash" id="pass_confirmIcon"></i>
+                                        </div>
+                                        <input type="password" name="" id="pass_confirm" class="form-control"
+                                            autocomplete="false">
+                                    </div>
+                                </div>
+                                <button class="btn btn-secondary mt-3 w-100" type="submit"><i
+                                        class="bi bi-box-arrow-in-right me-1"></i>
+                                    Sign Up Now</button>
+                                <div class="mt-5 text-center text-muted">
+                                    Already have an account?
+                                    <a href="/login" class="text-decoration-none fw-bold text-muted">
+                                        Login here
+                                    </a>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
     <script>
         function phoneNumber() {
-            // let first = $('#phone').val()
-            // console.log(first.substring(0, 1))
-            // if (first.substring(0, 1) == 0) {
-            //     let real = first.substring(2, 12)
-            //     $('#phone').val('628' + real)
-            // }
             $('#phone').val('+628')
+        }
 
+        function vPassword($id) {
+            var x = document.getElementById($id);
+            if (x.type === "password") {
+                x.type = "text";
+                $('#' + $id + 'Icon').removeClass('bi bi-eye-slash')
+                $('#' + $id + 'Icon').addClass('bi bi-eye')
+            } else {
+                x.type = "password";
+                $('#' + $id + 'Icon').removeClass('bi bi-eye')
+                $('#' + $id + 'Icon').addClass('bi bi-eye-slash')
+            }
         }
     </script>
 @endsection
